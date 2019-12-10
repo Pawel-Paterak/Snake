f217297007380bf048b90fe616f57311ad7a5d69
AT-15 <pawelpaterak@onet.pl>
2019-12-10 21:35:55 +0100

    *Created class for detection press key *Added directory for files controlers

--- Snake/Core.cs
+++ Snake/Core.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Snake.Controlers;
+using System;
 
 namespace Snake
 {
     public class Core
     {
+        private Game game;
+        private KeyboardControl keyboardControl = new KeyboardControl();
+
         public void Start()
         {
+            keyboardControl.Start();
             Console.ReadKey();
+            keyboardControl.Close();
         }
     }
 }
