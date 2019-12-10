0bd2ff640f0affc79df7bd5c7ef007845846a53c
AT-15 <pawelpaterak@onet.pl>
2019-12-10 21:05:41 +0100

    *Added directory for configuring classes *Created class for console configuration -Added option changing the console title -Added option changing the console size -Added option changing the console cursor visible

---
+++ Snake/Configurations/ConsoleConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Snake.Configurations
+{
+    public class ConsoleConfiguration
+    {
+        private const string title = "Snake";
+        private const int widht = 40;
+        private const int height = 40;
+        private const bool cursorVisible = false;
+
+        public void Configuration()
+        {
+            WindowTitle(title);
+            WindowsSize(widht, height);
+            CursorVisible(cursorVisible);
+        }
+
+        private void WindowTitle(string title)
+        {
+            Console.Title = title;
+        }
+
+        private void WindowsSize(int widht, int height)
+        {
+            Console.SetWindowSize(widht, height);
+            Console.SetBufferSize(widht, height);
+        }
+
+        private void CursorVisible(bool visible)
+        {
+            Console.CursorVisible = visible;
+        }
+    }
+}
