fe3544d861009f5cdbc67f98032218b054095048
AT-15 <pawelpaterak@onet.pl>
2020-01-01 20:26:24 +0100

    Added library Newtonsoft.Json Added file "File" and "Json" Added JsonManager class

---
+++ Snake/File/Json/JsonManager.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Snake.File.Json
+{
+    class JsonManager
+    {
+        public void Write(string path, object obj)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            using (StreamWriter sw = new StreamWriter(path))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                    serializer.Serialize(writer, obj);
+        }
+
+        public T Read<T>(string path)
+        {
+            T temp = default(T);
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            using (StreamReader sw = new StreamReader(path))
+                using (JsonReader reader = new JsonTextReader(sw))
+                    temp = (T)serializer.Deserialize(reader, typeof(T));
+            return temp;
+        }
+    }
+}
